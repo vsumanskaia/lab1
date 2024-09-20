dfffef2c810af549e1537875e2c717d027fe46fd
vsumanskaia <vsumanskaia@edu.hse.ru>
2024-09-20 20:13:57 +0300

    add main.cpp to repository

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab1code
+//
+//  Created by Vladislava Umanskaya on 20.09.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
